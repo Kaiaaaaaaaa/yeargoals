e1a2c2f4ad97761a3435f2a5e4a2c2df0cb2e77c
Kaiaaaaaaaa <kgiltun@gmail.com>
2026-02-13 21:33:26 +0100

    asfdsafdasf

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 let goal1 = false;
 let goal2 = false;
 let goal3 = false;
+
+document.getElementById("goal1").innerHTML = goal1 ? "☑︎ Hell yeah!" : "☐ Total failure!";
+document.getElementById("goal2").innerHTML = goal2 ? "☑︎ Aaaaw yeah!!" : "☐ Come on bruh...";
+document.getElementById("goal3").innerHTML = goal3 ? "☑︎ Let's goooo!" : "☐ Are you even trying?";
