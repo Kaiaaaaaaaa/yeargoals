fa8d140c822e9dc36ecfb8cc467abede8fdebd27
Kaiaaaaaaaa <kgiltun@gmail.com>
2026-02-14 16:22:15 +0100

    Added star counter

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let goal1 = false;
 let goal2 = false;
 let goal3 = false;
 
+let starCount = 7;
+
 document.getElementById("goal1").innerHTML = goal1 ? "☑︎ Hell yeah!" : "☐ Total failure!";
 document.getElementById("goal2").innerHTML = goal2 ? "☑︎ Aaaaw yeah!!" : "☐ Come on bruh...";
 document.getElementById("goal3").innerHTML = goal3 ? "☑︎ Let's goooo!" : "☐ Are you trying?";
@@ -12,9 +14,11 @@ window.addEventListener("click", function()
 {
     const newStar = document.createElement("div");
     newStar.classList.add("star");
-    newStar.style.left = Math.random() * window.innerWidth + "px";
-    newStar.style.top = Math.random() * window.innerHeight + "px";
+    newStar.style.left = Math.random() * window.innerWidth*2 + "px";
+    newStar.style.top = Math.random() * window.innerHeight*2 + "px";
     document.getElementById("mySky").appendChild(newStar);
+    starCount++;
+    updateStarCounter();
 });
 
 window.addEventListener("contextmenu", function(e) 
@@ -24,5 +28,10 @@ window.addEventListener("contextmenu", function(e)
     if (stars.length > 0) {
         document.getElementById("mySky").removeChild(stars[stars.length - 1]);
     }
+    starCount --;
+    updateStarCounter();
 });
 
+function updateStarCounter() {
+    document.getElementById("starCount").innerHTML = starCount;
+}
