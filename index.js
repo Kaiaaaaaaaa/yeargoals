6ed3c5d43bacf4b355977ac0c30f5c6d17af510e
Kaiaaaaaaaa <kgiltun@gmail.com>
2026-02-15 12:48:08 +0100

    Added goal 4

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 let goal1 = false;
 let goal2 = false;
 let goal3 = false;
+let goal4 = false;
 
 let starCount = 7;
 
 document.getElementById("goal1").innerHTML = goal1 ? "☑︎ Hell yeah!" : "☐ Total failure!";
 document.getElementById("goal2").innerHTML = goal2 ? "☑︎ Aaaaw yeah!!" : "☐ Come on bruh...";
 document.getElementById("goal3").innerHTML = goal3 ? "☑︎ Let's goooo!" : "☐ Are you trying?";
-
+document.getElementById("goal4").innerHTML = goal4 ? "☑︎ You're a star!" : "☐ Keep trying!";
 
 window.addEventListener("click", function(e) 
 {
